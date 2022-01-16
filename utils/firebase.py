61eb412c947af93c6dae53d0c69db5e05f81150a
Paul Clarke <paulioclarke@icloud.com>
2022-01-16 22:07:47 +1000

    Adds some firebase functions for interacting with the alliances table

--- utils/firebase.py
+++ utils/firebase.py
@@ -6,11 +6,7 @@ from datetime import datetime
 def find_fleet(fleet: Fleet):
     fleet_ref = db.collection('fleets').document(str(fleet.uid))
     doc = fleet_ref.get()
-
-    if doc.exists:
-        return True
-    else:
-        return False
+    return doc.exists
 
 def set_fleet(fleet: Fleet):
     data = {
@@ -50,3 +46,32 @@ def remove_fleet_uid(uid: int):
 # maybe store notifications as well?
 
 # store alliance user data as well...
+def get_all_alliances():
+    docs = db.collection('alliances').stream()
+    alliances = []
+    # exclude yourself from the alliance list
+    for a in docs:
+        if len(a.id) < 15:
+            info = a.to_dict()
+            alliances.append(a)
+    return alliances
+
+def is_alliance(uid: int): # checks if user is allied with you
+    fleet_ref = db.collection('is_alliance').document(str(uid))
+    doc = fleet_ref.get()
+    return doc.exists
+
+def remove_alliance(uid: int):
+    try:
+        db.collection('alliances').document(str(uid)).delete()
+    except Exception as e:
+        print(f'An exception occured: {e}')
+
+'''
+Gets the ally's information from the database. Will return their uid
+and their api_key to be used in any calculations. 
+'''
+def get_ally_info(uid: int):
+    fleet_ref = db.collection('is_alliance').document(str(uid))
+    doc = fleet_ref.get()
+    return doc.to_dict()
